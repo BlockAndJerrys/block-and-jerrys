f667209d9ca1e61c94d95bcf418dfd0e969ad4da
robertdurst <rsdurst@colby.edu>
2018-02-03 12:27:08 -0800

    Eslint applied and file styling cleaned up

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render( <MuiThemeProvider>
-      <App />
-    </MuiThemeProvider>, document.getElementById('root'));
+ReactDOM.render(
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
--- src/utils/menu.js
+++ src/utils/menu.js
@@ -1,22 +1,22 @@
 export default [
   {
-    img_url: "https://imgur.com/3ANZiYa.png",
-    flavor: "Ethereum Split",
+    img_url: 'https://imgur.com/3ANZiYa.png',
+    flavor: 'Ethereum Split',
     price: 0.000012,
   },
   {
-    img_url: "https://i.imgur.com/hVb71IB.png",
-    flavor: "Chunky Blocky",
+    img_url: 'https://i.imgur.com/hVb71IB.png',
+    flavor: 'Chunky Blocky',
     price: 0.000020,
   },
   {
-    img_url: "https://imgur.com/M0AFAjA.png",
-    flavor: "Doge Food",
+    img_url: 'https://imgur.com/M0AFAjA.png',
+    flavor: 'Doge Food',
     price: 0.000018,
   },
   {
-    img_url: "https://imgur.com/EsTI5i6.png",
+    img_url: 'https://imgur.com/EsTI5i6.png',
     flavor: "S'more Privacy",
     price: 0.000031,
   },
-]
+];
