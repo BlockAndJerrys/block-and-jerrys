004ddfa9c5181eda5ac2e21bdd1ef52b1bcd3372
jeff <tangj1122@gmail.com>
2018-02-18 22:40:00 -0800

    decent styling so far

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,27 +24,20 @@ import {
 } from 'react-bootstrap';
 
 import RaisedButton from 'material-ui/RaisedButton';
-import { GridTile, GridList } from 'material-ui/GridList';
 import Dialog from 'material-ui/Dialog';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import Right from 'material-ui/svg-icons/hardware/keyboard-arrow-right';
 
 import { connect } from 'react-redux';
 
 import '../styles/App.css';
 import logo from '../assets/logo.png';
 import Cart from './cart';
-// import menu from '../utils/menu';
 
-const styles = {
-  gridList: {
-    display: 'flex',
-    flexFlow: 'row nowrap',
-    marginTop: '1.5em',
-    overflowX: 'auto',
-  },
-  titleBackground: 'linear-gradient(to top, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)',
-};
+// const styles = {
+// };
 
-// const App = ({ socket, cart, coneCount, handleAdd, handleInit }) => {
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -63,74 +56,56 @@ class App extends React.Component {
 
   render() {
     return (
-      <Grid>
+      <Grid style={{ display: 'flex', flexFlow: 'column nowrap' }}>
         <Row>
-          <Col xs={1} style={{ backgroundColor: 'white' }}>
+          <Col xs={2} md={1} style={{ backgroundColor: 'white' }}>
             <a href="http://dev.lightning.community/" target="_blank" rel="noopener noreferrer">
               <Image responsive rounded src="https://github.com/lightningnetwork/lnd/raw/master/logo.png" alt="LND logo" />
             </a>
           </Col>
-          <Col xs={4} xsOffset={3}>
-            <Image responsive rounded src={logo} alt="LND logo" />
+          <Col xs={6} xsOffset={1} mdOffset={2} >
+            <Image responsive rounded src={logo} alt="LND logo" style={{ paddingTop: '0.5em' }} />
           </Col>
-          <Col xsOffset={2} xs={2} style={{ backgroundColor: 'white' }}>
-            Total Cones Bought: <b>{this.props.coneCount}</b>
+          <Col xsOffset={0} xs={3} md={1} mdOffset={2} style={{ backgroundColor: 'white', padding: '0' }}>
+            ConeCount: <b>{this.props.coneCount}</b>
           </Col>
         </Row>
-        <RaisedButton
-          label="Checkout"
-          secondary
-          onClick={this.handleOpen}
-          fullWidth
-        />
         <Row>
 
-          {
-            this.props.cart.map(x => (
-              <Col key={x.id} xs={8} sm={6} md={3}
-                style={{ display: 'flex', flexFlow: 'column nowrap', alignItems: 'center', justifyContent: 'center' }}
-              >
-                <Image src={x.img_url} />
-                <p>{x.flavor}</p>
-                <p>{x.price}</p>
-                <RaisedButton
-                  label="Add"
-                  secondary
-                  onClick={() => this.handleAdd({ id: x.id })}
-                />
-              </Col>
-              ))
-          }
+          <div style={{ paddingTop: '1em', marginBottom: '1em', display: 'flex', flexFlow: 'row nowrap', overflowX: 'auto', boxShadow: '3px 5px 6px black' }}>
+            {
+              this.props.cart.map(x => (
+                <Col key={x.id} xs={12} md={6}
+                  style={{ display: 'flex', flexFlow: 'column nowrap', alignItems: 'center', justifyContent: 'center', marginRight: '2px'  }}
+                >
+                  <Image src={x.img_url} style={{}}/>
+                  <div style={{ display: 'flex', background: 'linear-gradient(to top, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.5) 70%, rgba(0, 0, 0, 0.2) 100%)', position: 'absolute', bottom: '0', width: '100%', alignItems: 'center', justifyContent: 'center', color: 'white', fontSize: '3em', lineHeight: '1em' }}>
+                    <p>{x.flavor} <br /> <span style={{ fontSize: '0.5em', marginLeft: '0.5em' }}>${x.price}.00</span></p>
+                    <FloatingActionButton
+                      secondary
+                      mini
+                      onClick={() => this.props.handleAdd({ id: x.id })}
+                      zDepth={0}
+                      style={{ fontSize: '1rem' }}
+                    >
+                      <ContentAdd />
+                    </FloatingActionButton>
+                    { x.id < 4 && <Right color="white" style={{ position: 'absolute', right: '0', height: '1.1em', width: 'auto' }} /> }
+                  </div>
+                </Col>
+                ))
+            }
+          </div>
 
-          {/*
-          <GridList
-            padding={1}
-            cellHeight="auto"
-            style={{ display: 'flex', flexFlow: 'col wrap', overflowY: 'auto' }}
-          >
-            {this.props.cart.map(x => (
-              <GridTile
-                key={x.id}
-                titleBackground={styles.titleBackground}
-                children={<img src={x.img_url} alt={x.flavor} style={{ height: '70%' }} />}
-                actionIcon={
-                  <RaisedButton
-                    onClick={() => this.props.handleAdd({ id: x.id })}
-                    label="Add to Cart"
-                    fullWidth
-                    secondary
-                  />
-                }
-                title={x.flavor}
-                subtitle={`$${x.price}.00 USD`}
-              />
-            ))}
-          </GridList>
-          */}
         </Row>
         <Dialog open={this.state.open} onRequestClose={this.handleClose}>
           <Cart />
         </Dialog>
+        <RaisedButton
+          label="Checkout"
+          secondary
+          onClick={this.handleOpen}
+        />
         <Row>
           <Col>
             Use of this website constitutes your acceptance of Block And Jerry&#39;s <a href="/t-and-c">Terms & Conditions</a>.
